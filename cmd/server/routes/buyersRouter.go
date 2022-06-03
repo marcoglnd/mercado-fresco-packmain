f708adf299139eb4e0ade3aca6575aeac4bd5e5a
Marco Galindo <marcoantonio.galindo@mercadolivre.com>
2022-06-03 15:31:45 -0300

    feat: Cria rotas para o buyer

--- cmd/server/routes/buyersRouter.go
+++ cmd/server/routes/buyersRouter.go
@@ -1,18 +1,26 @@
 package routes
 
 import (
-	"net/http"
+	"github.com/marcoglnd/mercado-fresco-packmain/cmd/server/controllers/"
+	"github.com/marcoglnd/mercado-fresco-packmain/internal/buyers/"
 
 	"github.com/gin-gonic/gin"
 )
 
 func buyersRouter(superRouter *gin.RouterGroup) {
+	//1. repositório
+	repository := buyers.NewRepository()
+
+	//2. serviço (regra de negócio)
+	service := buyers.NewService(repository)
+
+	//3. controller
+	buyerController := controllers.NewBuyer(service)
 	pr := superRouter.Group("/buyers")
 	{
-		pr.GET("/debug", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusTeapot, gin.H{
-				"debug": "is running",
-			})
-		})
+		pr.GET("/", buyerController.GetAll())
+		pr.POST("/", buyerController.Create())
+		pr.PATCH("/:id", buyerController.Update())
+		pr.DELETE("/:id", buyerController.Delete())
 	}
 }
