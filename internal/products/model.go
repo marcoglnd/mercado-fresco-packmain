1fab7aed7db8355f9ab1cbbfb012dfd8a056630b
Julio Cesar Monteiro Alves <jumonteiro@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2022-06-03 12:50:44 -0300

    feat: product struct

--- internal/products/model.go
+++ internal/products/model.go
@@ -1,1 +1,16 @@
+package products
 
+type Product struct {
+	Id                             int     `json:"id"`
+	Description                    string  `json:"description"`
+	ExpirationRate                 int     `json:"expiration_rate"`
+	FreezingRate                   int     `json"freezing_rate"`
+	Height                         float64 `json:"height"`
+	Length                         float64 `json:"length"`
+	NetWeight                      float64 `json:"netweight"`
+	ProductCode                    string  `json:"product_code"`
+	RecommendedFreezingTemperature float64 `json:"recommended_freezing_temperature"`
+	Width                          float64 `json:"width"`
+	Product_typeId                 int     `json:"product_type_id"`
+	SellerId                       int     `json:"seller_id"`
+}
