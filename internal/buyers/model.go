e404401b9a18df7f6f89ff5553aeae1540e566c4
Marco Galindo <marcoantonio.galindo@mercadolivre.com>
2022-06-03 14:23:09 -0300

    feat: Adiciona model de Buyer

--- internal/buyers/model.go
+++ internal/buyers/model.go
@@ -1,1 +1,9 @@
+package buyers
 
+//Modelo de produtos
+type Buyer struct {
+	ID           int    `json:"id"`
+	CardNumberID string `json:"card_number_id"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`
+}
