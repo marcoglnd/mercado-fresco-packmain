d0172983883e3608481985cfb3bbda27470e159e
Julio Cesar Monteiro Alves <jumonteiro@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2022-06-03 14:21:15 -0300

    feat(repository): creating getall function

--- internal/products/repository.go
+++ internal/products/repository.go
@@ -1,2 +1,19 @@
 package products
 
+type Repository interface {
+	GetAll() ([]Product, error)
+}
+
+type repository struct{
+	db []Product
+}
+
+func (r *repository) GetAll() ([]Product, error) {
+	return r.db, nil
+}
+
+func NewRepository(db []Product) Repository {
+	return &repository{
+		db: db,
+	}
+}
