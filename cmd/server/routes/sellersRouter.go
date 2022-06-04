84addeb12bd50e07aba2df4c3bbc32f89d6b71d5
denisemignoli <denisemignoli@gmail.com>
2022-06-03 22:10:13 -0300

    criacao rotas getAll e post - sellers

--- cmd/server/routes/sellersRouter.go
+++ cmd/server/routes/sellersRouter.go
@@ -6,12 +6,54 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Seller struct {
+	ID           int    `json:"id"`
+	Cid          int    `json:"cid"`
+	Company_name string `json:"company_name"`
+	Adress       string `json:"adress"`
+	Telephone    int    `json:"telephone"`
+}
+
+type Sellers []Seller
+
+var lastID int
+var sellers Sellers
+
 func sellersRouter(superRouter *gin.RouterGroup) {
 	pr := superRouter.Group("/sellers")
 	{
-		pr.GET("/debug", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusTeapot, gin.H{
-				"debug": "is running",
+		pr.GET("/", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{"data": &sellers})
+		})
+
+		pr.GET("/id", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "is running",
+			})
+		})
+
+		pr.POST("/", func(ctx *gin.Context) {
+
+			var seller Seller
+			// tratando erro caso os dados não estiverem compatíveis com a estrutura esperada
+			if err := ctx.ShouldBindJSON(&seller); err != nil {
+				ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+				return
+			}
+
+			lastID++
+			seller.ID = lastID
+
+			sellers = append(sellers, seller)
+
+			/*
+				if seller.Company_name != "frutaria" {
+					ctx.JSON(http.StatusUnauthorized, gin.H{"status": "não autorizado"})
+					return
+				}
+			*/
+			ctx.JSON(http.StatusOK, gin.H{
+				"data": seller,
 			})
 		})
 	}
--- internal/sellers/model.go
+++ internal/sellers/model.go
@@ -1,1 +1,10 @@
+package sellers
 
+// Modelo de sellers
+type Seller struct {
+	ID           int    `json:"id"`
+	Cid          int    `json:"cid"`
+	Company_name string `json:"company_name"`
+	Adress       string `json:"adress"`
+	Telephone    int    `json:"telephone"`
+}
