4657ac11af0a2886079473d4cd4611a53c494588
denisemignoli <denisemignoli@gmail.com>
2022-06-05 00:38:18 -0300

    camada service

---
+++ internal/sellers/service.go
@@ -0,0 +1,45 @@
+package sellers
+
+type Service interface {
+	GetAll() ([]Seller, error)
+	Store(id int, cid int, company_name string, address string, telephone int) (Seller, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func (s service) GetAll() ([]Seller, error) {
+	sr, err := s.repository.GetAll()
+	if err != nil {
+		return nil, err
+	}
+	return sr, nil
+}
+
+func (s service) Store(id int, cid int, company_name string, address string, telephone int) (Seller, error) {
+	lastID, err := s.repository.LastID()
+
+	if err != nil {
+		return Seller{}, err
+	}
+
+	lastID++
+
+	seller, err := s.repository.Store(lastID, cid, company_name, address, telephone)
+
+	if err != nil {
+		return Seller{}, err
+	}
+
+	return seller, nil
+
+}
+
+// Recebe a interface como parâmetro
+
+func NewService(r Repository) Service {
+	return &service{
+		repository: r,
+	}
+}
