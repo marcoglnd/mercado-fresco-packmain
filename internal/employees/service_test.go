9c96acb09fb318bccc720aa06753426c33adeaaf
paloma-ribeiro <palomaribeiro.dev@gmail.com>
2022-06-17 20:16:48 -0300

    test: created methods for service_test.go

--- internal/employees/service_test.go
+++ internal/employees/service_test.go
@@ -1,1 +1,27 @@
 package employees
+
+import "github.com/stretchr/testify/mock"
+
+type EmployeeService struct {
+	mock.Mock
+}
+
+func (e *EmployeeService) GetAll() ([]Employee, error) {
+	return []Employee{}, nil
+}
+
+func (e *EmployeeService) GetById(id int) (Employee, error) {
+	return Employee{}, nil
+}
+
+func (e *EmployeeService) Create(cardNumberId, firstName, lastName string, warehouseId int) (Employee, error) {
+	return Employee{}, nil
+}
+
+func (e *EmployeeService) Update(id int, cardNumberId, firstName, lastName string, warehouseId int) (Employee, error) {
+	return Employee{}, nil
+}
+
+func (e *EmployeeService) Delete(id int) error {
+	return nil
+}
