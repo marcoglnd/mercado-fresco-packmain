be9b66c3acce3da32dce134b9490ee96270c3331
denise-mignoli <denise.mignoli@mercadolivre.com>
2022-06-18 15:28:33 -0300

    test update and delete - service

--- internal/sellers/service_test.go
+++ internal/sellers/service_test.go
@@ -174,3 +174,98 @@ func TestCreate(t *testing.T) {
 
 	})
 }
+
+func TestUpdate(t *testing.T) {
+	mock := new(mocks.Repository)
+
+	t.Run("Update data in case of success", func(t *testing.T) {
+		seller1 := createRandomSeller()
+		seller2 := createRandomSeller()
+
+		seller2.ID = seller1.ID
+
+		mock.On("Create",
+			seller1.Cid, seller1.Company_name, seller1.Address, seller1.Telephone,
+		).Return(seller1, nil).Once()
+		mock.On("Update",
+			seller1.ID, seller2.Cid, seller2.Company_name, seller2.Address, seller2.Telephone,
+		).Return(seller2, nil).Once()
+
+		s := NewService(mock)
+		newSeller1, err := s.Create(seller1.Cid, seller1.Company_name, seller1.Address, seller1.Telephone)
+		assert.NoError(t, err)
+		assert.NotEmpty(t, newSeller1)
+
+		assert.Equal(t, seller1, newSeller1)
+
+		newSeller2, err := s.Update(seller1.ID, seller2.Cid, seller2.Company_name, seller2.Address, seller2.Telephone)
+		assert.NoError(t, err)
+		assert.NotEmpty(t, newSeller2)
+
+		assert.Equal(t, seller1.ID, newSeller2.ID)
+		assert.NotEqual(t, seller1.Cid, newSeller2.Cid)
+		assert.NotEqual(t, seller1.Company_name, newSeller2.Company_name)
+		assert.NotEqual(t, seller1.Address, newSeller2.Address)
+		assert.NotEqual(t, seller1.Telephone, newSeller2.Telephone)
+
+		mock.AssertExpectations(t)
+	})
+
+	t.Run("Update throw an error in case of an nonexistent ID", func(t *testing.T) {
+		seller := createRandomSeller()
+
+		mock.On("Update", seller.ID, seller.Cid, seller.Company_name, seller.Address, seller.Telephone).Return(Seller{}, errors.New("failed to retrieve seller")).Once()
+
+		service := NewService(mock)
+
+		seller, err := service.Update(seller.ID, seller.Cid, seller.Company_name, seller.Address, seller.Telephone)
+
+		assert.Error(t, err)
+		assert.Empty(t, seller)
+
+		mock.AssertExpectations(t)
+	})
+}
+
+func TestDelete(t *testing.T) {
+	mock := new(mocks.Repository)
+
+	sellerArg := createRandomSeller()
+
+	t.Run("Delete in case of success", func(t *testing.T) {
+		mock.On("Create", sellerArg.Cid, sellerArg.Company_name, sellerArg.Address, sellerArg.Telephone).Return(sellerArg, nil).Once()
+		mock.On("GetAll").Return([]Seller{sellerArg}, nil).Once()
+		mock.On("Delete", sellerArg.ID).Return(nil).Once()
+		mock.On("GetAll").Return([]Seller{}, nil).Once()
+
+		service := NewService(mock)
+
+		newSeller, err := service.Create(sellerArg.Cid, sellerArg.Company_name, sellerArg.Address, sellerArg.Telephone)
+		assert.NoError(t, err)
+		list1, err := service.GetAll()
+		assert.NoError(t, err)
+		err = service.Delete(newSeller.ID)
+		assert.NoError(t, err)
+		list2, err := service.GetAll()
+		assert.NoError(t, err)
+
+		assert.NotEmpty(t, list1)
+		assert.NotEqual(t, list1, list2)
+		assert.Empty(t, list2)
+
+		mock.AssertExpectations(t)
+
+	})
+
+	t.Run("Delete in case of error", func(t *testing.T) {
+		mock.On("Delete", 185).Return(errors.New("seller's ID not founded")).Once()
+
+		service := NewService(mock)
+
+		err := service.Delete(185)
+
+		assert.Error(t, err)
+
+		mock.AssertExpectations(t)
+	})
+}
