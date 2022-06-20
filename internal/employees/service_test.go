b310dbb0758091f36ac04fd5dde8ce353fbdcad2
paloma-ribeiro <palomaribeiro.dev@gmail.com>
2022-06-20 10:32:13 -0300

    feat: create func TestGetAll

--- internal/employees/service_test.go
+++ internal/employees/service_test.go
@@ -1,29 +1,72 @@
-package employees
+package employees_test
 
 import (
-	"github.com/stretchr/testify/mock"
+	"errors"
+	"testing"
+
+	. "github.com/marcoglnd/mercado-fresco-packmain/internal/employees"
+	"github.com/marcoglnd/mercado-fresco-packmain/internal/employees/mocks"
+	"github.com/marcoglnd/mercado-fresco-packmain/utils"
+	"github.com/stretchr/testify/assert"
 )
 
-type EmployeeService struct {
-	mock.Mock
+func createRandomEmployee() (employee Employee) {
+	employee = Employee{
+		ID:           1,
+		CardNumberId: utils.RandomCategory(),
+		FirstName:    utils.RandomCategory(),
+		LastName:     utils.RandomCategory(),
+		WarehouseId:  utils.RandomCode(),
+	}
+	return
 }
 
-func (e *EmployeeService) GetAll() ([]Employee, error) {
-	return []Employee{}, nil
+func createRandomListEmployee() (listOfEmployee []Employee) {
+
+	for i := 1; i <= 5; i++ {
+		employee := createRandomEmployee()
+		employee.ID = i
+		listOfEmployee = append(listOfEmployee, employee)
+	}
+	return
 }
 
-func (e *EmployeeService) GetById(id int) (Employee, error) {
-	return Employee{}, nil
-}
+func TestGetAll(t *testing.T) {
+	mock := new(mocks.Repository)
 
-func (e *EmployeeService) Create(cardNumberId, firstName, lastName string, warehouseId int) (Employee, error) {
-	return Employee{}, nil
-}
+	employeesArg := createRandomListEmployee()
 
-func (e *EmployeeService) Update(id int, cardNumberId, firstName, lastName string, warehouseId int) (Employee, error) {
-	return Employee{}, nil
-}
+	t.Run("GetAll in case of success", func(t *testing.T) {
+		mock.On("GetAll").Return(employeesArg, nil)
 
-func (e *EmployeeService) Delete(id int) error {
-	return nil
+		service := NewService(mock)
+
+		list, err := service.GetAll()
+
+		assert.NoError(t, err)
+		assert.NotEmpty(t, list)
+
+		for i := 0; i < len(employeesArg); i++ {
+			assert.Equal(t, employeesArg[i].ID, list[i].ID)
+			assert.Equal(t, employeesArg[i].CardNumberId, list[i].CardNumberId)
+			assert.Equal(t, employeesArg[i].FirstName, list[i].FirstName)
+			assert.Equal(t, employeesArg[i].LastName, list[i].LastName)
+			assert.Equal(t, employeesArg[i].WarehouseId, list[i].WarehouseId)
+		}
+		mock.AssertExpectations(t)
+	})
+
+	t.Run("GetAll in case of error", func(t *testing.T) {
+		mock.On("GetAll").Return(nil, errors.New("failed to retrieve employees"))
+
+		service := NewService(mock)
+
+		list, err := service.GetAll()
+
+		assert.Error(t, err)
+		assert.Empty(t, list)
+
+		mock.AssertExpectations(t)
+	})
+
 }
