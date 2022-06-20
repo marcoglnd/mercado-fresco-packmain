6e1d98854888f14cbe9c37302a4f3c4138634d41
juliocesar1402 <jumonteiro@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2022-06-20 15:05:05 -0300

    test(createProduct): status created

--- cmd/server/controllers/products_test.go
+++ cmd/server/controllers/products_test.go
@@ -4,10 +4,12 @@ import (
 	"bytes"
 	"net/http"
 	"net/http/httptest"
+	"testing"
 
 	"github.com/gin-gonic/gin"
 	"github.com/marcoglnd/mercado-fresco-packmain/cmd/server/controllers"
 	"github.com/marcoglnd/mercado-fresco-packmain/internal/products"
+	"github.com/stretchr/testify/assert"
 )
 
 func createServer() *gin.Engine {
@@ -40,4 +42,26 @@ func createRequestTest(
 	req.Header.Add("Content-Type", "application/json")
 
 	return req, httptest.NewRecorder()
+}
+
+func TestCreateProductOK(t *testing.T) {
+	r := createServer()
+
+	req, rr := createRequestTest(http.MethodPost, "/products/", `{
+		"description": "Yogurt",
+		"expiration_rate": 1,
+		"freezing_rate": 2,
+		"height": 6.4,
+		"length": 4.5,
+		"netweight": 3.4,
+		"product_code": "PROD01",
+		"recommended_freezing_temperature": 1.3,
+		"width": 1.2,
+		"product_type_id": 2,
+		"seller_id": 2
+		}`)
+
+	r.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusCreated, rr.Code)
 }
