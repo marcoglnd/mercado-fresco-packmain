3a43d947b7884943e296673f458435797da31161
juliocesar1402 <julio.monteiro@mercadolivre.com>
2022-06-30 18:39:52 -0300

    fix: env file name

--- utils/config.go
+++ utils/config.go
@@ -14,7 +14,7 @@ type Config struct {
 // LoadConfig reads configuration from file or environment variables
 func LoadConfig(path string) (config Config, err error) {
 	viper.AddConfigPath(path)
-	viper.SetConfigName("app")
+	viper.SetConfigName(".env")
 	viper.SetConfigType("env")
 
 	viper.AutomaticEnv()
