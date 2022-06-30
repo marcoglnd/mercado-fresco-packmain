f07a00c6d1cbc0d475b5561f6c019dd560955a00
juliocesar1402 <julio.monteiro@mercadolivre.com>
2022-06-30 19:39:25 -0300

    feat(products/repository): implementing db into update function

--- internal/products/repository.go
+++ internal/products/repository.go
@@ -75,27 +75,6 @@ func (r *repository) GetById(ctx context.Context, id int) (*Product, error) {
 	return &product, nil
 }
 
-func (repository) LastId() (int, error) {
-	if len(listOfProducts) == 0 {
-		return 1, nil
-	}
-	lastId := listOfProducts[len(listOfProducts)-1].Id + 1
-	return int(lastId), nil
-}
-
-func (r *repository) VerifyProductCode(productCode string) (bool, error) {
-	list, err := r.GetAll()
-	if err != nil {
-		return false, err
-	}
-	for _, prod := range list {
-		if prod.ProductCode == productCode {
-			return false, fmt.Errorf("product_code already used")
-		}
-	}
-	return true, nil
-}
-
 func (r *repository) CreateNewProduct(ctx context.Context, product *Product) (*Product, error) {
 	newProduct := Product{}
 	result, err := r.db.ExecContext(
@@ -124,36 +103,39 @@ func (r *repository) CreateNewProduct(ctx context.Context, product *Product) (*P
 	return &newProduct, nil
 }
 
-func (repository) Update(
-	id int, description string, expirationRate, freezingRate int,
-	height, length, netWeight float64, productCode string,
-	recommendedFreezingTemperature, width float64, productTypeId, sellerId int) (Product, error) {
-	prod := Product{
-		Id:                             int64(id),
-		Description:                    description,
-		ExpirationRate:                 expirationRate,
-		FreezingRate:                   freezingRate,
-		Height:                         height,
-		Length:                         length,
-		NetWeight:                      netWeight,
-		ProductCode:                    productCode,
-		RecommendedFreezingTemperature: recommendedFreezingTemperature,
-		Width:                          width,
-		ProductTypeId:                  productTypeId,
-		SellerId:                       sellerId,
+func (r *repository) Update(ctx context.Context, product *Product) (*Product, error) {
+	newProduct := Product{}
+
+	result, err := r.db.ExecContext(
+		ctx,
+		sqlUpdate,
+		&product.Description,
+		&product.ExpirationRate,
+		&product.FreezingRate,
+		&product.Height,
+		&product.Length,
+		&product.NetWeight,
+		&product.ProductCode,
+		&product.RecommendedFreezingTemperature,
+		&product.Width,
+		&product.ProductTypeId,
+		&product.SellerId,
+		&product.Id,
+	)
+	if err != nil {
+		return &newProduct, err
 	}
-	updated := false
-	for i := range listOfProducts {
-		if listOfProducts[i].Id == int64(id) {
-			prod.Id = int64(id)
-			listOfProducts[i] = prod
-			updated = true
-		}
+
+	affectedRows, err := result.RowsAffected()
+	if affectedRows == 0 {
+		return &newProduct, errors.New("section id not found")
 	}
-	if !updated {
-		return Product{}, fmt.Errorf("product %d not found", id)
+
+	if err != nil {
+		return &newProduct, err
 	}
-	return prod, nil
+
+	return product, nil
 }
 
 func (repository) Delete(id int) error {
