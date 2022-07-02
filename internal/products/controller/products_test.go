5c0ebef943df1f27198b884b811b591d4ec69861
juliocesar1402 <julio.monteiro@mercadolivre.com>
2022-07-02 12:12:26 -0300

    test(products/controller): refactoring createNewProduct tests

---
+++ internal/products/controller/products_test.go
@@ -0,0 +1,101 @@
+package controller
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/marcoglnd/mercado-fresco-packmain/internal/products/domain"
+	"github.com/marcoglnd/mercado-fresco-packmain/internal/products/domain/mocks"
+	"github.com/marcoglnd/mercado-fresco-packmain/utils"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestCreateNewProduct(t *testing.T) {
+	mockProduct := utils.CreateRandomProduct()
+
+	productsServiceMock := mocks.NewService(t)
+
+	t.Run("success", func(t *testing.T) {
+
+		productsServiceMock.On("CreateNewProduct",
+			mock.Anything,
+			mock.Anything,
+		).Return(&mockProduct, nil).Once()
+
+		payload, err := json.Marshal(mockProduct)
+		assert.NoError(t, err)
+
+		req := httptest.NewRequest(http.MethodPost, "/api/v1/products", bytes.NewBuffer(payload))
+		rec := httptest.NewRecorder()
+
+		_, engine := gin.CreateTestContext(rec)
+
+		productController := Controller{service: productsServiceMock}
+
+		engine.POST("/api/v1/products", productController.CreateNewProduct())
+
+		engine.ServeHTTP(rec, req)
+
+		assert.Equal(t, http.StatusCreated, rec.Code)
+
+		productsServiceMock.AssertExpectations(t)
+	})
+
+	t.Run("fail with unprocessable entity", func(t *testing.T) {
+		mockProductBad := &domain.Product{}
+
+		productsServiceMock.On("CreateNewProduct",
+			mock.Anything,
+			mock.Anything,
+		).Return(mockProductBad, errors.New("bad request")).Maybe()
+
+		payload, err := json.Marshal(mockProductBad)
+		assert.NoError(t, err)
+
+		req := httptest.NewRequest(http.MethodPost, "/api/v1/products", bytes.NewBuffer(payload))
+		rec := httptest.NewRecorder()
+
+		_, engine := gin.CreateTestContext(rec)
+
+		productController := Controller{service: productsServiceMock}
+
+		engine.POST("/api/v1/products", productController.CreateNewProduct())
+
+		engine.ServeHTTP(rec, req)
+
+		assert.Equal(t, http.StatusUnprocessableEntity, rec.Code)
+
+		productsServiceMock.AssertExpectations(t)
+	})
+
+	t.Run("fail with status conflict", func(t *testing.T) {
+		productsServiceMock.On("CreateNewProduct",
+			mock.Anything,
+			mock.Anything,
+		).Return(nil, errors.New("expected conflict error")).Maybe()
+
+		payload, err := json.Marshal(mockProduct)
+		assert.NoError(t, err)
+
+		req := httptest.NewRequest(http.MethodPost, "/api/v1/products", bytes.NewBuffer(payload))
+		rec := httptest.NewRecorder()
+
+		_, engine := gin.CreateTestContext(rec)
+
+		productController := Controller{service: productsServiceMock}
+
+		engine.POST("/api/v1/products", productController.CreateNewProduct())
+
+		engine.ServeHTTP(rec, req)
+
+		assert.Equal(t, http.StatusConflict, rec.Code)
+
+		productsServiceMock.AssertExpectations(t)
+	})
+}
