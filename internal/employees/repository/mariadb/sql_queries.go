ec5370c070897e98938e40dc757d8c6e8e5cabab
paloma-ribeiro <palomaribeiro.dev@gmail.com>
2022-07-04 15:50:23 -0300

    fix: correction sql queries in file sql_queries.go

--- internal/employees/repository/mariadb/sql_queries.go
+++ internal/employees/repository/mariadb/sql_queries.go
@@ -1,33 +1,13 @@
 package mariadb
 
 const (
-	queryGetAll = `SELECT 
-	id
-	card_number_id
-	first_name
-	last_name
-	warehouse_id
-	FROM employees`
+	sqlGetAll = `SELECT * FROM employees`
 
-	queryGetById = `SELECT 
-	id
-	card_number_id
-	first_name
-	last_name
-	warehouse_id
-	FROM employees
-	WHERE ID = ?`
+	sqlGetById = `SELECT * FROM employees WHERE ID = ?`
 
-	queryCreate = `INSERT INTO employees 
-	(card_number_id, first_name, last_name, warehouse_id) 
-	VALUES (?, ?, ?, ?)`
+	sqlInsert = `INSERT INTO employees (card_number_id, first_name, last_name, warehouse_id) VALUES (?, ?, ?, ?)`
 
-	queryUpdate = `UPDATE employees SET
-	card_number_id=?, 
-	first_name=?, 
-	last_name=?, 
-	warehouse_id=?
-	WHERE ID =?`
+	sqlUpdate = `UPDATE employees SET card_number_id = ?, first_name = ?, last_name = ?, warehouse_id = ? WHERE ID = ?`
 
-	queryDelete = `DELETE FROM employees WHERE id=?`
+	sqlDelete = `DELETE FROM employees WHERE id=?`
 )
