2114baca342c52b57102e4e5a6ebfb207cd49e93
paloma-ribeiro <palomaribeiro.dev@gmail.com>
2022-07-04 11:09:27 -0300

    feat: uploaded file employee_errors.go

--- internal/employees/domain/employee_errors.go
+++ internal/employees/domain/employee_errors.go
@@ -3,6 +3,10 @@ package domain
 import "errors"
 
 var (
-	ErrIdNotFound     = errors.New("employee id not found")
-	ErrInvalidService = errors.New("invalid service")
+	ErrIdNotFound               = errors.New("employee id not found")
+	ErrInvalidService           = errors.New("invalid service")
+	CardNumberIdIsRequired      = errors.New("card_number_id is required")
+	FirstNameIsRequired         = errors.New("first_name is required")
+	LastNameIsRequired          = errors.New("last_name is required")
+	WarehouseIdCannotBeNegative = errors.New("warehouse_id cannot be negative")
 )
