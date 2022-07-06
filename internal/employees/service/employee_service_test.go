5934b998d5b9a5cf14d7ba580369ee2ac31c9eb8
paloma-ribeiro <palomaribeiro.dev@gmail.com>
2022-07-06 18:32:32 -0300

    test: created TestGetById for service

--- internal/employees/service/employee_service_test.go
+++ internal/employees/service/employee_service_test.go
@@ -61,7 +61,6 @@ func TestGetAll(t *testing.T) {
 		newEmployees, err := service.GetAll(context.Background())
 
 		assert.NoError(t, err)
-
 		assert.Equal(t, &mockEmployees, newEmployees)
 
 		mockEmployeeRepository.AssertExpectations(t)
@@ -80,5 +79,42 @@ func TestGetAll(t *testing.T) {
 
 		mockEmployeeRepository.AssertExpectations(t)
 
+	})
+}
+
+func TestGetBiId(t *testing.T) {
+	t.Run("In case of success", func(t *testing.T) {
+		mockEmployeeRepository := mocks.NewEmployeeRepository(t)
+		mockEmployee := utils.CreateRandomEmployee()
+
+		mockEmployeeRepository.On("GetById", mock.Anything,
+			mock.AnythingOfType("int64"),
+		).Return(&mockEmployee, nil).Once()
+
+		service := NewEmployeeService(mockEmployeeRepository)
+		employee, err := service.GetById(context.Background(), mockEmployee.ID)
+
+		assert.NoError(t, err)
+		assert.NotEmpty(t, employee)
+		assert.Equal(t, &mockEmployee, employee)
+
+		mockEmployeeRepository.AssertExpectations(t)
+	})
+
+	t.Run("In case of error", func(t *testing.T) {
+		mockEmployeeRepository := mocks.NewEmployeeRepository(t)
+		mockEmployee := utils.CreateRandomEmployee()
+
+		mockEmployeeRepository.On("GetById", mock.Anything,
+			mock.AnythingOfType("int64"),
+		).Return(nil, errors.New("failed to retrieve employee")).Once()
+
+		service := NewEmployeeService(mockEmployeeRepository)
+		employee, err := service.GetById(context.Background(), mockEmployee.ID)
+
+		assert.Error(t, err)
+		assert.Empty(t, employee)
+
+		mockEmployeeRepository.AssertExpectations(t)
 	})
 }
