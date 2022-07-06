6775548cfece1e01bd36e53cf897046a23cbf290
paloma-ribeiro <palomaribeiro.dev@gmail.com>
2022-07-06 18:47:50 -0300

    test: created TestDelete for service

--- internal/employees/service/employee_service_test.go
+++ internal/employees/service/employee_service_test.go
@@ -118,3 +118,37 @@ func TestGetBiId(t *testing.T) {
 		mockEmployeeRepository.AssertExpectations(t)
 	})
 }
+
+func TestDelete(t *testing.T) {
+	t.Run("In case of success", func(t *testing.T) {
+		mockEmployeeRepository := mocks.NewEmployeeRepository(t)
+		mockEmployee := utils.CreateRandomEmployee()
+
+		mockEmployeeRepository.On("Delete", mock.Anything,
+			mock.AnythingOfType("int64"),
+		).Return(nil).Once()
+
+		service := NewEmployeeService(mockEmployeeRepository)
+		err := service.Delete(context.Background(), mockEmployee.ID)
+
+		assert.NoError(t, err)
+
+		mockEmployeeRepository.AssertExpectations(t)
+	})
+
+	t.Run("In case of error", func(t *testing.T) {
+		mockEmployeeRepository := mocks.NewEmployeeRepository(t)
+		mockEmployee := utils.CreateRandomEmployee()
+
+		mockEmployeeRepository.On("Delete", mock.Anything,
+			mock.AnythingOfType("int64"),
+		).Return(errors.New("employee's ID not founded")).Once()
+
+		service := NewEmployeeService(mockEmployeeRepository)
+		err := service.Delete(context.Background(), mockEmployee.ID)
+
+		assert.Error(t, err)
+
+		mockEmployeeRepository.AssertExpectations(t)
+	})
+}
