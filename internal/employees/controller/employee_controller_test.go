aeb97d4b4984d9507e0998b4d854c2674a5e592d
paloma-ribeiro <palomaribeiro.dev@gmail.com>
2022-07-07 16:31:08 -0300

    test: created TestCreateNewEmployee in employee_controller_test.go

--- internal/employees/controller/employee_controller_test.go
+++ internal/employees/controller/employee_controller_test.go
@@ -1,1 +1,46 @@
 package controller
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/marcoglnd/mercado-fresco-packmain/internal/employees/domain/mocks"
+	"github.com/marcoglnd/mercado-fresco-packmain/utils"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestCreateNewEmployee(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		mockEmployee := utils.CreateRandomEmployee()
+		mockEmployeeService := mocks.NewEmployeeService(t)
+
+		mockEmployeeService.On("Create",
+			mock.Anything,
+			mock.Anything,
+		).Return(&mockEmployee, nil).Once()
+
+		payload, err := json.Marshal(mockEmployee)
+		assert.NoError(t, err)
+
+		req := httptest.NewRequest(http.MethodPost, "/api/v1/employees", bytes.NewBuffer(payload))
+		rec := httptest.NewRecorder()
+
+		_, engine := gin.CreateTestContext(rec)
+
+		employeeController := EmployeeController{service: mockEmployeeService}
+
+		engine.POST("/api/v1/employees", employeeController.Create())
+
+		engine.ServeHTTP(rec, req)
+
+		assert.Equal(t, http.StatusCreated, rec.Code)
+
+		mockEmployeeService.AssertExpectations(t)
+
+	})
+}
